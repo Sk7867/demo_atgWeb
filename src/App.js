66e0a076a1c056982c5d2b3574896e9d4a8b7f5b
Siddhesh Kamble <siddhesh.kamble7867@gmail.com>
2021-07-20 15:21:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/header';
+import Main from './components/main';
+import './css/style.min.css';
+import { useState } from 'react';
 function App() {
+  const [join, setJoin] = useState('join');
+
+  function isJoin(leave) {
+    leave === 'leave' ? setJoin('leave') : setJoin('join');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header join={join} isJoin={isJoin} />
+      <Main join={join} isJoin={isJoin} />
     </div>
   );
 }
